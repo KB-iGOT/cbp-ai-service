a65d6b74b1e5ea5dc092672cad5bf8c17f5c4d69
saipraneeth.meduri <saipraneeth.meduri@tarento.com>
2026-05-06 12:49:17 +0530

    fix: remove trailing comma from __all__ declaration in __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,4 @@ from .user import User
 from .login_attempt import LoginAttempt
 from .user_session import UserSession
 
-__all__ = ["User", "LoginAttempt", "UserSession",]
+__all__ = ["User", "LoginAttempt", "UserSession"]
